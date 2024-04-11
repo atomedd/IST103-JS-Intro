819ddccfc026dc4c3e9f1697dbbf2782ac714244
atomedd <edmar.viannas@gmail.com>
2024-04-10 19:39:19 -0700

    add person description

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 
-    alert("Hello World!");
+ //   alert("Hello World!"); 
+
+let personName = "Edmar Vianna";
+
+let personAge = 28;
+
+let isMarried = false;
+
+let personHobbies = ["gaming", "drawing", "movies"];
+
+let personAddress = {
+    street: "123 Some St",
+    city: "Vancouver",
+    state: "BC",
+    zip: "V0A 0A0"
+};
+
+let undefinedVar;
+
+let nullVar = null;
+
+const MAX_LOGIN_ATTEMPTS = 5;
